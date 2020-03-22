dad7c7318e66ee638179e8c4e9342774643ce81b
swetmore <61606854+swetmore@users.noreply.github.com>
2020-03-22 14:05:10 -0500

    Carousel update

--- src/Pages/videopage.jsx
+++ src/Pages/videopage.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import SimpleSliderContainer from '../Components/SimpleSliderContainer';
+import React, { Fragment } from "react";
+import "semantic-ui-css/semantic.min.css";
+import "react-multi-carousel/lib/styles.css";
+import Cat1 from "../Components/vcategory1";
+import Cat2 from "../Components/vcategory2";
+import Cat3 from "../Components/vcategory3";
+import Section from "../Components/videosection";
 
+const VideoPage = () => {
+    return (
+        <div>
+            <Fragment>
+                <Section>
+                    First Category
+                    <  Cat1 />
+                </Section>
+                <Section>
+                    Second Category
+                    <Cat2/>
+        </Section>
+                <Section>
+                    Third Category
+                    <Cat3/>
+        </Section>
+            </Fragment>
+        </div>
+    );
+};
 
-
-export default class VideoPage extends React.Component
-{
-    render(props) {
-        return (
-        <div> 
-        <SimpleSliderContainer></SimpleSliderContainer>
-
-    </div>
-        );
-    };
-}
+export default VideoPage;
