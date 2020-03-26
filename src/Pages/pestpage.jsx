49c934fadbbd3e21c7c0cd5f969ab72158d43fb9
swetmore <61606854+swetmore@users.noreply.github.com>
2020-03-26 16:19:21 -0500

    SearchTable

--- src/Pages/pestpage.jsx
+++ src/Pages/pestpage.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import PestSearchForm from '../Components/pestsearchform'
-import PestDisplay from '../Components/pestdisplay'
+import PestTable from '../Components/searchtable'
 
 class PestPage extends React.Component {
 
     render() {
         return (
             <div className="Pest">
-                <h5>Pest Search</h5>
-                <div>
-                    <PestSearchForm pestSearch/>
-                </div>
-                <PestDisplay></PestDisplay>
+                <PestTable></PestTable>
             </div>
         );
     }
