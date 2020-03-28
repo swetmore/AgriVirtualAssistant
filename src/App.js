1abf4595ed077a397848f952408072b56b608fcd
ktruong1s <54959881+ktruong1s@users.noreply.github.com>
2020-03-28 11:40:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
 import MainPage from "./Pages/homepage";
 import WeatherPage from "./Pages/weatherpage";
-import VideoPage from "./Pages/videopage";
 import PestPage from "./Pages/pestpage";
 import ErrorPage from './Pages/errorpage';
 import Navbar from './Components/navigationbar';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import VideoPage1 from './Pages/videopage1'
+import VideoPage2 from './Pages/videopage2'
+import VideoPage3 from './Pages/videopage3'
 
 const theme = createMuiTheme({
   palette: {
@@ -32,7 +34,9 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/" component={MainPage} />
               <Route exact path="/weatherpage" component={WeatherPage} />
-              <Route exact path="/videopage" component={VideoPage} />
+              <Route exact path="/videopage1" component={VideoPage1} />
+              <Route exact path="/videopage2" component={VideoPage2} />
+              <Route exact path="/videopage3" component={VideoPage3} />
               <Route exact path="/pestpage" component={PestPage} />
               <Route exact path="/errorpage" component={ErrorPage} />
               <Redirect to="/errorpage" />
