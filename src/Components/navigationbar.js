691715bd004f05c6fbbd69190e6763b1e209beaf
ktruong1s <54959881+ktruong1s@users.noreply.github.com>
2020-03-28 11:41:44 -0500

    Update navigationbar.js

--- src/Components/navigationbar.js
+++ src/Components/navigationbar.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import Link from 'react-router-dom/Link';
-//MUI
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-class Navbar extends Component {
-    render() {
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function Navbar()
+{
+     
+        const [anchorEl, setAnchorEl] = React.useState(null);
+          
+        const handleClick = event => {
+          setAnchorEl(event.currentTarget);
+        };
+      
+        const handleClose = () => {
+          setAnchorEl(null);
+        };
+
         return (
+
             <AppBar color='primary'>
                 <Toolbar>
                     <Button color="inherited" component={Link} to="/">Home</Button>
                     <Button color="inherited" component={Link} to="/weatherpage">Check the Weather</Button>
-                    <Button color="inherited" component={Link} to="/videopage">Watch Videos</Button>
+                    <Button aria-controls="simple-menu" aria-haspopup="true" color="inherited" onClick={handleClick}>
+                    Videos
+                    </Button>
+
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >   
+                        <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage1">Preservation</MenuItem>
+                        <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage2" >Productivity</MenuItem>
+                        <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage3">AG Technique</MenuItem>
+                    </Menu>
+                   
                     <Button color="inherited" component={Link} to="/pestpage">Search Pests</Button>
                 </Toolbar>
             </AppBar>
         )
     }
-}
-
-export default Navbar
