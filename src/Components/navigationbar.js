e5200f56269ccc7a440e7df2c05bae19fe3b90f7
swetmore <61606854+swetmore@users.noreply.github.com>
2020-03-28 19:13:21 -0500

    Update navigationbar.js

--- src/Components/navigationbar.js
+++ src/Components/navigationbar.js
@@ -3,46 +3,75 @@ import Link from 'react-router-dom/Link';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
-export default function Navbar()
-{
-     
-        const [anchorEl, setAnchorEl] = React.useState(null);
-          
-        const handleClick = event => {
-          setAnchorEl(event.currentTarget);
-        };
-      
-        const handleClose = () => {
-          setAnchorEl(null);
-        };
+export default function Navbar() {
 
-        return (
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-            <AppBar color='primary'>
-                <Toolbar>
-                    <Button color="inherited" component={Link} to="/">Home</Button>
-                    <Button color="inherited" component={Link} to="/weatherpage">Check the Weather</Button>
-                    <Button aria-controls="simple-menu" aria-haspopup="true" color="inherited" onClick={handleClick}>
-                    Videos
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+
+        <AppBar color='primary'>
+            <Toolbar>
+                <Button color="inherit" component={Link} to="/"><Typography variant="h6" color="inherit">
+                    Home </Typography></Button>
+                <Button color="inherit" component={Link} to="/weatherpage"><Typography variant="h6" color="inherit">
+                    Check the Weather</Typography></Button>
+                <Button aria-controls="simple-menu" aria-haspopup="true" color="inherited" onClick={handleClick}>
+                <Typography variant="h6" color="inherit">
+                    Videos</Typography>
                     </Button>
 
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >   
-                        <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage1">Preservation</MenuItem>
-                        <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage2" >Productivity</MenuItem>
-                        <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage3">AG Technique</MenuItem>
-                    </Menu>
-                   
-                    <Button color="inherited" component={Link} to="/pestpage">Search Pests</Button>
-                </Toolbar>
-            </AppBar>
-        )
-    }
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage1">Preservation</MenuItem>
+                    <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage2" >Productivity</MenuItem>
+                    <MenuItem onClick={handleClose} color="inherited" component={Link} to="/videopage3">AG Technique</MenuItem>
+                </Menu>
+
+                <Button color="inherit" component={Link} to="/pestpage"><Typography variant="h6" color="inherit">
+                    Search Pests</Typography></Button>
+                <Button color="inherit" component={Link} to="/photopage"><Typography variant="h6" color="inherit">
+                    Pest Photos</Typography></Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+// class Navbar extends Component {
+//     render() {
+//         return (
+//             <AppBar color='primary'>
+//                 <Toolbar>
+//                     <Button color="inherit" component={Link} to="/"><Typography variant="h6" color="inherit">
+//                         Home </Typography></Button>
+//                     <Button color="inherit" component={Link} to="/weatherpage"><Typography variant="h6" color="inherit">
+//                         Check the Weather</Typography></Button>
+//                     <Button color="inherit" component={Link} to="/videopage"> <Typography variant="h6" color="inherit">
+//                         Watch Videos</Typography></Button>
+//                     <Button color="inherit" component={Link} to="/pestpage"><Typography variant="h6" color="inherit">
+//                         Search Pests</Typography></Button>
+//                     <Button color="inherit" component={Link} to="/photopage"><Typography variant="h6" color="inherit">
+//                         Pest Photos</Typography></Button>
+//                 </Toolbar>
+//             </AppBar>
+//         )
+//     }
+// }
+
+// export default Navbar
