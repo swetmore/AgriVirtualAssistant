cb0595dcda53a0d3528c4e3c77f44d134cfe6066
swetmore <61606854+swetmore@users.noreply.github.com>
2020-03-29 16:14:03 -0500

    Update pestpage.jsx

--- src/Pages/pestpage.jsx
+++ src/Pages/pestpage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Helmet} from 'react-helmet'
 import PestTable from '../Components/searchtable'
-
 class PestPage extends React.Component {
 
     render() {
         return (
             <div className="Pest">
+                <Helmet>
+                   <style>{'body {background-color: #bbdefb; }'}</style>
+               </Helmet>
                 <PestTable></PestTable>
             </div>
         );
