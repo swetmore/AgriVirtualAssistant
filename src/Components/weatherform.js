cc2d39fa4854e1c6f3ed4beeb4cdcc6d0b1071e7
swetmore <61606854+swetmore@users.noreply.github.com>
2020-04-04 20:37:35 -0500

    Update weatherform.js

--- src/Components/weatherform.js
+++ src/Components/weatherform.js
@@ -1,11 +1,11 @@
 import React from "react"
-
+import '../weather.css'
 class Form extends React.Component {
     render() {
         return (
             <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..." />
-                <input type="text" name="state" placeholder="State..." />
+                <input className = "weatherinput" type="text" name="city" placeholder="City..." />
+                <input className = "weatherinput" type="text" name="state" placeholder="State..." />
                 <button>Show Weather</button>
             </form>
         );
